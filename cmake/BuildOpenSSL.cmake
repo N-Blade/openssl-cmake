881fdac56952d0d9328fcc65ebe23d80f2eafa1b
Dmitry Gomzyakov <kyoto1337@protonmail.com>
2022-12-28 17:44:11 +0500

    remove unneeded var

--- cmake/BuildOpenSSL.cmake
+++ cmake/BuildOpenSSL.cmake
@@ -31,13 +31,6 @@ include(ExternalProject)
 find_package(Git REQUIRED)
 find_package(PythonInterp 3 REQUIRED)
 
-# # used to apply various patches to OpenSSL
-find_program(PATCH_PROGRAM patch)
-if (NOT PATCH_PROGRAM)
-    message(FATAL_ERROR "Cannot find patch utility. This is only required for Android cross-compilation but due to script complexity "
-                        "the requirement is always enforced")
-endif()
-
 # set variables
 ProcessorCount(NUM_JOBS)
 set(OS "UNIX")
